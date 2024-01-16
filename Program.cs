c01495b59b5b7c2884fe5863ce7e64984f5542df
Dingless <mohameddeg.abdulahi@elev.ga.ntig.se>
2024-01-16 15:39:23 +0100

    gjort denna

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿
+
+
+
+Console.WriteLine("Ange ditt användarnamn");
+string användarnamn = Console.ReadLine();
+Console.WriteLine("Ange ditt lösenord");
+string lösenord = Console.ReadLine();
+if (användarnamn == "noname" && lösenord == "nopass")
+{
+    Console.WriteLine("Välkommen");
+}
+else
+{
+    Console.WriteLine("Fel användarnamn eller lösenord");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
